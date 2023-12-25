8a7aeb32ef65e829c9e4e90c84a045c43e9494e4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-25 23:09:13 +0100

    created an array for 12 cards element

---
+++ app.js
@@ -0,0 +1,53 @@
+const cardArray = [
+  {
+    myName: "fries",
+    img: "images/fries.jpg",
+  },
+  {
+    myName: "cheeseburger",
+    img: "images/cheeseburger.jpg",
+  },
+  {
+    myName: "hotdog",
+    img: "images/hotdog.jpg",
+  },
+  {
+    myName: "icecream",
+    img: "images/icecream.jpg",
+  },
+  {
+    myName: "milkshake",
+    img: "images/milkshake.jpg",
+  },
+  {
+    myName: "pizza",
+    img: "images/pizza.jpg",
+  },
+  {
+    myName: "fries",
+    img: "images/fries.jpg",
+  },
+  {
+    myName: "cheeseburger",
+    img: "images/cheeseburger.jpg",
+  },
+  {
+    myName: "hotdog",
+    img: "images/hotdog.jpg",
+  },
+  {
+    myName: "icecream",
+    img: "images/icecream.jpg",
+  },
+  {
+    myName: "milkshake",
+    img: "images/milkshake.jpg",
+  },
+  {
+    myName: "pizza",
+    img: "images/pizza.jpg",
+  },
+];
+
+//to sort the cards randomly
+cardArray.sort(() => 0.5 - Math.random());
